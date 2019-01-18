a455090421a5879e893f050ce0a19a6f6e62a8ff
Ian Chouinard <ianchouinard@gmail.com>
2019-01-18 02:11:35 -0500

    Updates

---
+++ precache-manifest.0aa5a30d9a61e6d7eb7aef6ddf7b6098.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "adf0fc5148e103d7da7024b776264afd",
+    "url": "/react-weather/static/media/status_most_clouds.adf0fc51.svg"
+  },
+  {
+    "revision": "c48bd991cd7f6c5e0e9f",
+    "url": "/react-weather/static/css/main.682cf85d.chunk.css"
+  },
+  {
+    "revision": "852c972c5c57e2a24bca",
+    "url": "/react-weather/static/js/1.852c972c.chunk.js"
+  },
+  {
+    "revision": "8ad3cef1c2506df250b9",
+    "url": "/react-weather/static/js/runtime~main.8ad3cef1.js"
+  },
+  {
+    "revision": "e8d1f913b04eef209e3dafb088347a54",
+    "url": "/react-weather/static/media/header_bg.e8d1f913.jpg"
+  },
+  {
+    "revision": "ffb6fb3dbf9106e05ad903db3fd52466",
+    "url": "/react-weather/static/media/status_clouds.ffb6fb3d.svg"
+  },
+  {
+    "revision": "dcea1ca75274cee058e352c030549c3c",
+    "url": "/react-weather/static/media/status_sunny.dcea1ca7.svg"
+  },
+  {
+    "revision": "c48bd991cd7f6c5e0e9f",
+    "url": "/react-weather/static/js/main.c48bd991.chunk.js"
+  },
+  {
+    "revision": "14d54e242ba9313158e2bf4b953def26",
+    "url": "/react-weather/static/media/status_rain.14d54e24.svg"
+  },
+  {
+    "revision": "dd4939e963fc01dcf95865bb532b8924",
+    "url": "/react-weather/static/media/status_snow.dd4939e9.svg"
+  },
+  {
+    "revision": "4146cdf069fcb631398ff31273aecd80",
+    "url": "/react-weather/static/media/status_storm.4146cdf0.svg"
+  },
+  {
+    "revision": "8a9ae1baa5d98706d5abf4d1451d0cbb",
+    "url": "/react-weather/static/media/status_fog.8a9ae1ba.svg"
+  },
+  {
+    "revision": "8932d3900cf7b41c2a0660ee16fd12e5",
+    "url": "/react-weather/static/media/arrow.8932d390.svg"
+  },
+  {
+    "revision": "0aa0c33c4e81144adb2505f379539ef8",
+    "url": "/react-weather/index.html"
+  }
+];
