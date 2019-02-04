cadda56a8d8071eda32be4bebcd7867edecabb8a
Ian Chouinard <ianchouinard@gmail.com>
2019-02-03 19:06:25 -0500

    Teste

--- src/api/GeocodeApi.js
+++ src/api/GeocodeApi.js
@@ -1,4 +1,4 @@
-const apiKey = 'AIzaSyClE4bKdB1dSdpUNAq1SGQ0OOzZcbbUDb4'
+const apiKey
 
 const locationInfoUrl = (location) => {
   return `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${apiKey}`
